f4e26577c5a9268adcb5ed27237eefaee1b5678c
CaptainProton42 <john.wigg@gmx.net>
2020-11-28 14:55:24 +0100

    Fixed collision of multiple CircleShapes.

--- scripts/classes/SoundShape.gd
+++ scripts/classes/SoundShape.gd
@@ -4,14 +4,14 @@ class_name SoundShape
 export var audio_stream : AudioStream
 
 func get_area() -> float:
-    return 0.0
+	return 0.0
 
 func get_class() -> String:
-    return "SoundShape"
+	return "SoundShape"
 
 func _ready():
-    $AudioStreamPlayer.stream = audio_stream
+	$AudioStreamPlayer.stream = audio_stream
 
 func emit_sound(pitch : float = 1.0):
-    $AudioStreamPlayer.pitch_scale = pitch
-    $AudioStreamPlayer.play()
+	$AudioStreamPlayer.pitch_scale = pitch
+	$AudioStreamPlayer.play()
