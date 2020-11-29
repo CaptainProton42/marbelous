90ff31302ff1c329350d3d1316db03e2897e7c3a
CawawaC <cawawac@gmail.com>
2020-11-29 10:57:11 +0100

    collectible sound

--- scripts/classes/Collectible.gd
+++ scripts/classes/Collectible.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name Collectible
 
+export (Array, AudioStream) var collect_sounds
+
 signal picked_up
 
 enum Type {
@@ -16,6 +18,14 @@ func _on_body_entered(body) -> void:
 	body.collect(self)
 	$AnimationPlayer.play("pickup")
 	emit_signal("picked_up")
+	
+	if has_node("sound"):
+		var i = body._collected_nodes.size()-1
+		if i < collect_sounds.size():
+			$sound.stream = collect_sounds[i]
+		else:
+			$sound.stream = collect_sounds.back()
+		$sound.play()
 
 func get_class() -> String:
 	return "Collectible"
