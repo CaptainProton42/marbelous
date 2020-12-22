dfa7f443e4dab9cb9852c433b2aef85f0cfe7c95
CawawaC <cawawac@gmail.com>
2020-12-22 17:29:32 +0100

    new levels

--- scripts/level list.gd
+++ scripts/level list.gd
@@ -1,12 +1,20 @@
 extends Node
 
 const list = [
-	"Level verybasic",
-	"Level0",
-	"Level 2sides 1note wwait",
-	"Level1",
-	"Level invisible shapes",
-	"1 marble",
-	"marble lifetime",
-	"decay pass"
+	"nothing",
+	"sidestep",
+	"bump in the road",
+	"launch",
+	"horseshoe",
+	"straight lock",
+	"side key"
+#
+#	"Level verybasic",
+#	"Level0",
+#	"Level 2sides 1note wwait",
+#	"Level1",
+#	"Level invisible shapes",
+#	"1 marble",
+#	"marble lifetime",
+#	"decay pass"
 ]
